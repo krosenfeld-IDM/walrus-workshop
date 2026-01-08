7169ae141ddc10e63301ae8a771e7f060ba367b4
Katherine Rosenfeld <krosenf@gmail.com>
2026-01-08 06:51:36 -0800

    original sparsify training example

---
+++ experiments/sparsify/example.py
@@ -0,0 +1,25 @@
+import torch
+from datasets import load_dataset
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from sparsify import SaeConfig, Trainer, TrainConfig
+from sparsify.data import chunk_and_tokenize
+
+MODEL = "HuggingFaceTB/SmolLM2-135M"
+dataset = load_dataset(
+    "EleutherAI/SmolLM2-135M-10B", split="train",
+)
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+tokenized = chunk_and_tokenize(dataset, tokenizer)
+
+
+gpt = AutoModelForCausalLM.from_pretrained(
+    MODEL,
+    device_map={"": "cuda"},
+    torch_dtype=torch.bfloat16,
+)
+
+cfg = TrainConfig(SaeConfig(), batch_size=16)
+trainer = Trainer(cfg, tokenized, gpt)
+
+trainer.fit()
